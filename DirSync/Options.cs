0fba4872dbd37bd2120fac329c9d701ad3fded80
Alexander Satov <aleksandr-satov@mail.ru>
2018-08-25 15:39:39 +0500

    added console logging

---
+++ DirSync/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace DirSync
+{
+    public class Options
+    {
+        [Option('s', "source", Required = true, HelpText = "Source directory for synchronizing")]
+        public string Source { get; set; }
+
+        [Option('t', "target", Required = true, HelpText = "Target directory which will be synchronized with source directory")]
+        public string Target { get; set; }
+    }
+}
--- DirSync/Program.cs
+++ DirSync/Program.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommandLine;
+using DirSync.Log;
 
 namespace DirSync
 {
     public class Program
     {
+        private static readonly ILog log = new SyncLog();
+
         public static void Main(string[] args)
         {
+            try
+            {
+                Parser.Default.ParseArguments<Options>(args)
+                    .WithParsed(Execute)
+                    .WithNotParsed(HandleErrors);
+            }
+            catch (Exception e)
+            {
+                log.Error($"Unknown error:\r\n{e}");
+                Environment.Exit(-1);
+            }
+        }
 
+        private static void Execute(Options options)
+        {
+            log.Info($"Start synchronizing {options.Target} with {options.Source}");
+        }
+
+        private static void HandleErrors(IEnumerable<Error> errors)
+        {
+            foreach (var error in errors)
+            {
+                log.Error($"Arguments parsing error:\r\n{error}");
+            }
         }
     }
 }
