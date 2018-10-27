c03f46a349bed934b548696e3ef3869eba6b3a1e
Alexander Satov <aleksandr-satov@mail.ru>
2018-10-27 19:59:48 +0500

    log fix

---
+++ DirSync/Logging/ConsoleLog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DirSync.Log
+{
+    public class ConsoleLog : ILog
+    {
+        public void AddInfo(string filename)
+        {
+            LogMessage($"Added \"{filename}\"", ConsoleColor.Green);
+        }
+
+        public void DeleteInfo(string filename)
+        {
+            LogMessage($"Deleted \"{filename}\"", ConsoleColor.Red);
+        }
+
+        public void ReplaceInfo(string filename)
+        {
+            LogMessage($"Replaced \"{filename}\"", ConsoleColor.Cyan);
+        }
+
+        public void Info(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            LogMessage(message, color);
+        }
+
+        public void Warn(string message)
+        {
+            LogMessage(message, ConsoleColor.Yellow);
+        }
+
+        public void Error(string message)
+        {
+            LogMessage(message, ConsoleColor.DarkRed);
+        }
+
+        private static void LogMessage(string message, ConsoleColor color)
+        {
+            var consoleColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = consoleColor;
+        }
+    }
+}
---
+++ DirSync/Logging/ILog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DirSync.Log
+{
+    public interface ILog
+    {
+        void AddInfo(string filename);
+        void DeleteInfo(string filename);
+        void ReplaceInfo(string filename);
+
+        void Info(string message, ConsoleColor color = ConsoleColor.White);
+        void Warn(string message);
+        void Error(string message);
+    }
+}
